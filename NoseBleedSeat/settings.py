cb43b450d1110aeb37dbc201372a94104f7386dd
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2024-11-18 00:07:12 +0200

    Update settings.py

--- NoseBleedSeat/settings.py
+++ NoseBleedSeat/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
