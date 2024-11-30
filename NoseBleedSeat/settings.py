cd1cfbd2fc8819ac919573734dfb4005304e2470
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2024-11-30 16:45:33 +0200

    Update settings.py
    updated settings.py

--- NoseBleedSeat/settings.py
+++ NoseBleedSeat/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# imports
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
